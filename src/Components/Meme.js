c88de261788107f294d8c522f9db457789282e65
Stephanie Mitchell <stephsorandom@gmail.com>
2022-02-24 17:58:27 -0500

    api

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -4,7 +4,7 @@ import React, {useState, useEffect} from "react"
 function Meme() {
     const [meme, setMeme] = useState({topText: "", bottomText: "",randomImage: "http://i.imgflip.com/1bij.jpg" })
     const [allMemes, setAllMemes] = useState([])
-    const [memeImage, setMemeImage] = useState('')
+  
 
 
     function getMemeImage() {
@@ -37,7 +37,7 @@ function Meme() {
                 <input type="text" placeholder="Top text" className="form-input" name="topText" value={meme.topText}onChange={handleChange}/>
 
                 <input type="text" placeholder="Bottom text" className="form-input" name="bottomText" value={meme.bottomText} onChange={handleChange}/>
-                
+
                 <button className="form-button" onClick={getMemeImage}>
                     Get a new meme image 🖼
                 </button>
